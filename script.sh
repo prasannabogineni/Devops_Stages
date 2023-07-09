33d5948dac2c70d437c04eb1c3ff4173a2906a7d
root <root@ip-172-31-86-49.ec2.internal>
2023-07-09 05:53:44 +0000

    updated script file after gave a absolute path in Build step

--- script.sh
+++ script.sh
@@ -2,3 +2,4 @@
 
 echo "Hello prassu......Welcome to the Jenkins environmnt"
 echo "Hello im changing content from terminal to git for the automatic build checking"
+echo "Hello world from 'git branch' branch"
